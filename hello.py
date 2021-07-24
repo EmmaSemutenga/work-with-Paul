670cbc594782d3071102966d2dab346499a4d29b
Emmanuel Semutenga <emzsema@gmail.com>
2021-07-24 20:03:26 +0300

    Created the hello file

---
+++ hello.py
@@ -0,0 +1,25 @@
+# print("Hello World")
+
+# myage = input("How old are you?")
+# print(type(myage))
+# print(myage.capitalize())
+
+# print(type(print))
+print(type(678))
+# str(678)
+# Number: integers, floats(int, float), immutable
+# booleans: True, False()bool
+# print(4+False)
+# Immutable Sequence types
+# String(str)
+myname = 'Johnn'
+print(len(myname))
+# print(myname[2])
+# myname[2] = "w"
+# newname = myname[:2] + "w" + myname[3:]
+newname = myname.replace("h", "w")
+print(newname)
+print(myname)
+
+
+
